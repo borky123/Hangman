d0f0a2b839f2ca3aec9089793a04ed4314ee4c13
borky123 <86001952+borky123@users.noreply.github.com>
2022-01-17 09:31:46 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -21,20 +21,6 @@ words = [
     "BARBEQUE",
 
 ]
-
-function createButtons() {
-    chooseWord();
-    console.log(choosedWord);
-    for (i = 65; i <= 90; ++i) {
-        var letter = String.fromCharCode(i);
-        var div = document.getElementById("lettersButtons");
-        div.innerHTML += "<button class='keyboard' id='" + letter +  
-        "' onClick= 'letterCheck(this.id)'>" + letter + "</button>" + " ";
-    }
-    document.getElementById("startButton").disabled = true;
-    document.getElementById("livesLeft").style.visibility = "visible";
-}
-
 function chooseWord() {
     choosedWord = words[Math.floor(Math.random() * words.length)];
     //CREATE THE LETTER BOXES FOR SEARCHED WORD
@@ -42,6 +28,20 @@ function chooseWord() {
         var div = document.getElementById("searchedWord");
         div.innerHTML += "<div class='box' id='" + i + "'></div>";
     }
+}
+
+function createButtons() {
+    chooseWord();
+    console.log(choosedWord);
+    var div = document.getElementById("lettersButtons");
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('').map(letter => {
+        div.innerHTML += "<button class='keyboard' id='" + letter +  
+        "' onClick= 'letterCheck(this.id)'>" + letter + "</button>" + " ";
+    })
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("livesLeft").style.visibility = "visible";
+    document.getElementById("hangmanImg").style.visibility = "visible";
+
 }
 
 function letterCheck(btnValue) {
@@ -52,7 +52,7 @@ function letterCheck(btnValue) {
             findedPos = true;
             ++wonCounter;
             if (wonCounter == choosedWord.length) {
-                gameWon();
+                gameFinished();
             }           
         }
     }
@@ -60,30 +60,34 @@ function letterCheck(btnValue) {
         document.getElementById("lives").textContent = --lives;
         document.getElementById(btnValue).style.backgroundColor = "#e95f5a";
         if (lives == 0) {
-            gameOver();
+            gameFinished();
         }
     }
+    var img = document.getElementById("hangmanImg");
+    img.innerHTML = '<img src="man' + lives + '.png">';
     document.getElementById(btnValue).disabled = true;
     console.log(choosedWord);
 }
 
-function gameOver() {
-    document.getElementById("gameFinished").style.visibility = "visible";
-    document.getElementById("gameFinished").textContent = "GAME OVER !!!";
-    document.getElementById("correctAnswer").style.visibility = "visible";
-    document.getElementById("correctAnswer").textContent += "Correct answer is: " + choosedWord;
-    document.getElementById("searchedWord").style.visibility = "hidden";
-    document.querySelectorAll('button.keyboard').forEach(elem => {
-        elem.disabled = true;
-    });
-}
-
-function gameWon() {
-    document.getElementById("gameFinished").style.visibility = "visible";
-    document.getElementById("gameFinished").textContent = "WOW!! You WON ";
-    document.getElementById("correctAnswer").style.visibility = "visible";
-    document.getElementById("correctAnswer").textContent += choosedWord;
-    document.getElementById("searchedWord").style.visibility = "hidden";
-    document.querySelectorAll('button.keyboard').forEach(elem => {
-        elem.disabled = true;});
+function gameFinished() {
+    if (lives == 0) {
+    //GAMEOVER
+        document.getElementById("gameFinished").style.visibility = "visible";
+        document.getElementById("gameFinished").textContent = "GAME OVER !!!";
+        document.getElementById("correctAnswer").style.visibility = "visible";
+        document.getElementById("correctAnswer").textContent += "Correct answer is: " + choosedWord;
+        document.getElementById("searchedWord").style.visibility = "hidden";
+        document.querySelectorAll('button.keyboard').forEach(elem => {
+            elem.disabled = true;}); 
     }
+    if (wonCounter == choosedWord.length) {
+    //GAMEWON
+        document.getElementById("gameFinished").style.visibility = "visible";
+        document.getElementById("gameFinished").textContent = "WOW!! You WON ";
+        document.getElementById("correctAnswer").style.visibility = "visible";
+        document.getElementById("correctAnswer").textContent += choosedWord;
+        document.getElementById("searchedWord").style.visibility = "hidden";
+        document.querySelectorAll('button.keyboard').forEach(elem => {
+            elem.disabled = true;});
+    }
+}
